316425dfb264320f48b92ec2f21ff227a95ca300
Makenna Worley <makennaworley@gmail.com>
2024-06-11 15:31:19 -0600

    Edges have minimal collision detection
    Edit the width's and height's of each node, that could help with collision detection

--- src/canvas/Canvas.js
+++ src/canvas/Canvas.js
@@ -1,99 +1,86 @@
 import './Canvas.css';
-
 import React, { useCallback, useState } from 'react';
 import ReactFlow, {
-  Background,
-  BackgroundVariant,
-  useNodesState,
-  useEdgesState,
-  addEdge,
-  applyEdgeChanges,
-  applyNodeChanges,
-  Position, 
-  getSmoothStepPath
+    Background,
+    BackgroundVariant,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    applyEdgeChanges,
+    applyNodeChanges,
+    Position,
+    getSmoothStepPath
 } from 'reactflow';
- 
+
 import 'reactflow/dist/style.css';
 import { ANDGateNode, ORGateNode, XORGateNode, NORGateNode, NANDGateNode, NOTGateNode, XNORGateNode } from './GateNode';
 import CustomStepEdge from './CustomEdge';
+import CustomSmartEdge from './CustomSmartEdge';
 
 const rfStyle = {
-  backgroundColor: '#00b5e25e',
+    backgroundColor: '#00b5e25e',
 };
- 
-const initialNodes = [
-  { id: 'node-1', type: 'ANDGateNode', position: { x: 0, y: -40 } },
-  { id: 'node-2', type: 'ORGateNode', position: { x: -50, y: 100 } },
-  { id: 'node-3', type: 'XORGateNode', position: { x: 100, y: 100 } },
-  { id: 'node-4', type: 'NORGateNode', position: { x: -200, y: 100 } },
-  { id: 'node-5', type: 'NANDGateNode', position: { x: -150, y: -40 } },
-  { id: 'node-6', type: 'NOTGateNode', position: { x: 150, y: -40 } },
-  { id: 'node-7', type: 'XNORGateNode', position: { x: 250, y: 100 } },
 
+const initialNodes = [
+    { id: 'node-1', type: 'ANDGateNode', position: { x: 0, y: -40 }, width: 110, height: 80 },
+    { id: 'node-2', type: 'ORGateNode', position: { x: -50, y: 100 }, width: 110, height: 80 },
+    { id: 'node-3', type: 'XORGateNode', position: { x: 100, y: 100 }, width: 110, height: 80 },
+    { id: 'node-4', type: 'NORGateNode', position: { x: -200, y: 100 }, width: 110, height: 80 },
+    { id: 'node-5', type: 'NANDGateNode', position: { x: -150, y: -40 }, width: 110, height: 80 },
+    { id: 'node-6', type: 'NOTGateNode', position: { x: 150, y: -40 }, width: 110, height: 80 },
+    { id: 'node-7', type: 'XNORGateNode', position: { x: 250, y: 100 }, width: 110, height: 80 },
 ];
 
 const initialEdges = [
-    { id: 'e1-2', source: 'node-1', target: 'node-2', type: 'customStepEdge', style: { stroke: 'red'} }, // Set custom edge type
-    { id: 'e3-6', source: 'node-3', target: 'node-6', type: 'customStepEdge', style: { stroke: 'red'} }
-  ];
+    { id: 'e1-2', source: 'node-1', target: 'node-2', type: 'CustomStepEdge', style: { stroke: 'red'} },
+    { id: 'e3-6', source: 'node-3', target: 'node-6', type: 'CustomSmartEdge', style: { stroke: 'green'}, data: { nodes: initialNodes } }
+];
 
-//defining customized node types
-const nodeTypes = { ANDGateNode: ANDGateNode, 
-                    ORGateNode: ORGateNode,
-                    XORGateNode: XORGateNode,
-                    NORGateNode: NORGateNode,
-                    NANDGateNode: NANDGateNode,
-                    NOTGateNode: NOTGateNode,
-                    XNORGateNode: XNORGateNode, };
+const nodeTypes = {
+    ANDGateNode,
+    ORGateNode,
+    XORGateNode,
+    NORGateNode,
+    NANDGateNode,
+    NOTGateNode,
+    XNORGateNode,
+};
 
-const source = { x: 0, y: 20 };
-const target = { x: 150, y: 100 };
-
-// Custom Edge Component
-
-const edgeTypes = { customStepEdge: CustomStepEdge }; //Register the custom edge
-
-
+const edgeTypes = { CustomStepEdge, CustomSmartEdge };
 
 function Canvas() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
+    const [nodes, setNodes] = useState(initialNodes);
+    const [edges, setEdges] = useState(initialEdges);
 
-  const onNodesChange = useCallback(
-    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    [setNodes]
-  );
-  const onEdgesChange = useCallback(
-    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    [setEdges]
-  );
-  const onConnect = useCallback(
-    (connection) => setEdges((eds) => addEdge({ ...connection, type: 'customStepEdge' }, eds)),
-    [setEdges]
-  );
-  return (
-    <div style={{ width: '75vw', height: '90vh', borderRadius: '3px' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        nodeTypes={nodeTypes}
-        edgeTypes={edgeTypes}
-        fitView
-        style={rfStyle}  
-      >
-        {/* <Background
-          id="1"
-          gap={30}
-          color="#f1f1f1"
-          variant={BackgroundVariant.Lines}
-        /> */}
-      </ReactFlow>
-    </div>
-  );
+    const onNodesChange = useCallback(
+        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [setNodes]
+    );
+    const onEdgesChange = useCallback(
+        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges]
+    );
+    const onConnect = useCallback(
+        (connection) => setEdges((eds) => addEdge({ ...connection, type: 'CustomSmartEdge', data: { nodes } }, eds)),
+        [setEdges, nodes]
+    );
+
+    return (
+        <div style={{ width: '75vw', height: '90vh', borderRadius: '3px' }}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                nodeTypes={nodeTypes}
+                edgeTypes={edgeTypes}
+                fitView
+                style={rfStyle}
+            >
+            </ReactFlow>
+        </div>
+    );
 }
-
 
 export default Canvas;
