b83ca5cb1e7ed734bf3e6d4cf2f666532a761f5d
Kathryn Lenth <kathryn.lenth@gmail.com>
2024-06-25 12:08:32 -0600

    Added ability to drag CircuitEdges

---
+++ src/canvas/MouseResponsiveEdge.js
@@ -0,0 +1,42 @@
+import { BaseEdge } from 'react-flow-renderer';
+
+// based on https://stackoverflow.com/questions/77831116/is-it-possible-to-shape-reactflow-edges-by-dragging-them#78118229
+
+export const MouseResponsiveEdge = ({
+    id,
+    path,
+    style,
+    markerEnd,
+    markerStart,
+    interactionWidth = 20,
+    onClick = (() => {}),
+    onPointerDown = (() => {}),
+    onPointerUp = (() => {}),
+    onPointerMove = (() => {}),
+}) => {
+    return (
+        <>
+            <path
+                id={id}
+                style={style}
+                d={path}
+                fill={'none'}
+                className={'react-flow__edge-path'}
+                markerEnd={markerEnd}
+                markerStart={markerStart}
+
+            />
+            {interactionWidth && (
+                <path
+                    d={path}
+                    fill={'none'}
+                    strokeWidth={interactionWidth}
+                    onClick={e => onClick(e)}
+                    onPointerDown={e => onPointerDown(e)}
+                    onPointerUp={e  => onPointerUp(e)}
+                    onPointerMove={e => onPointerMove(e)}
+                />
+            )}
+        </>
+    );
+};
