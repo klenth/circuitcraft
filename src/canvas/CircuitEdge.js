b471d28a0707c15122310e1b29ce661bf0d7f43f
kathy <klenth@westminstercollege.edu>
2024-07-18 09:45:16 -0600

    Refactor: moved routeEdge() and related code to src/circuits/routing.js

--- src/canvas/CircuitEdge.js
+++ src/canvas/CircuitEdge.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { EdgeLabelRenderer, useReactFlow, useNodes } from "reactflow";
 import { MouseResponsiveEdge } from './MouseResponsiveEdge';
 import { useRotation } from './RotationContext';
-
+import { routeEdge, renderPath } from '../circuits/routing.js';
 
 const CircuitEdge = ({
                            id,
@@ -226,59 +226,6 @@ function newDrag({ event, points, flow }) {
     return null;
 }
 
-const routeEdge = ({
-    sourceX, sourceY,
-    targetX, targetY,
-    sourcePosition, targetPosition
-}) => {
-    const sourceP = new Point(sourceX, sourceY);
-    const targetP = new Point(targetX, targetY);
-    const sourceNormal = PlusI; //positionVector(sourcePosition);
-    const targetNormal = MinusI; //positionVector(targetPosition);
-    const displacement = sourceP.displacement(targetP);
-    const behind = displacement.dot(sourceNormal) >= 0;
-    const parallel = sourceNormal.dot(targetNormal) !== 0.0;
-
-    const points = [ sourceP ];
-    const numInternalSegments = 2 + (parallel ? 1 : 0);
-
-    let horizontal = behind ?
-        sourceNormal.dot(PlusI) !== 0 :
-        sourceNormal.dot(PlusJ) !== 0
-    ;
-
-    const numHorizontalSegments = Math.floor((numInternalSegments + (horizontal ? 1 : 0)) / 2);
-    const numVerticalSegments = numInternalSegments - numHorizontalSegments;
-
-    let lastPoint = sourceP;
-    for (let i = 0; i < numInternalSegments; ++i, horizontal = !horizontal) {
-        let nextPoint;
-        if (horizontal)
-            nextPoint = lastPoint.displaced(PlusI.times(displacement.x / numHorizontalSegments));
-        else
-            nextPoint = lastPoint.displaced(PlusJ.times(displacement.y / numVerticalSegments));
-
-        points.push(nextPoint);
-        lastPoint = nextPoint;
-    }
-
-    const pathString = renderPath({
-        sourceX, sourceY,
-        points,
-        targetX, targetY
-    });
-
-    return { svgPathString: pathString, error: null, points: points, };
-};
-
-const renderPath = ({ sourceX, sourceY, points, targetX, targetY }) => {
-    let pathString = `M${sourceX},${sourceY}`;
-    for (const point of points)
-        pathString += `L${point.x},${point.y}`;
-    pathString += `L${targetX},${targetY}`;
-
-    return pathString;
-};
 
 const applyDrag = ({
     points, drag
@@ -297,51 +244,5 @@ const applyDrag = ({
 
     return newPoints;
 };
-
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    displacement(p) {
-        return new Vector(p.x - this.x, p.y - this.y);
-    }
-
-    displaced(v) {
-        return new Point(this.x + v.x, this.y + v.y);
-    }
-}
-
-class Vector {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    times(f) {
-        return new Vector(f * this.x, f * this.y);
-    }
-
-    dot(v) {
-        return this.x * v.x + this.y * v.y;
-    }
-}
-
-function positionVector(position) {
-    const map = { top: PlusJ, bottom: MinusJ, left: MinusI, right: PlusI };
-    if (map[position])
-        return map[position];
-    else {
-        console.debug(`Unknown position: ${position}`);
-        return PlusI; // default to rightward
-    }
-}
-
-// Standard basis vectors: +/-i (right/left), +/-j (up/down)
-const PlusI = new Vector(1, 0),
-      MinusI = new Vector(-1, 0),
-      PlusJ = new Vector(0, 1),
-      MinusJ = new Vector(0, -1);
 
 export default CircuitEdge;
