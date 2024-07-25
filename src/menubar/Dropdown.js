6a75398aafa855e5e60d6655cefe8305baf3a6ec
Shreeya Maskey <shreeyamaskey.1@gmail.com>
2024-07-25 11:30:46 -0600

    committing for help

--- src/menubar/Dropdown.js
+++ src/menubar/Dropdown.js
@@ -8,22 +8,24 @@ export function FileDropdown(props) {
     const exportPng = props.exportPng || (() => {})
     return (
         <>
-            <ul className={dropdown ? "dropdown clicked" : "dropdown"} onClick={() => setDropdown(!dropdown)}>
+            <ul className={dropdown ? "dropdown clicked" : "file_dropdown"} onClick={() => setDropdown(!dropdown)}>
                 <li className='dropdown_item'>New</li>
-                <li className='dropdown_item' onClick={() => { setDropdown(false); onSave(); }}>Save</li>
-                <li className='dropdown_item' onClick={() => { setDropdown(false); onOpen(); }}>Open</li>
+                <li className='dropdown_item' onClick={() => { onSave() }}>Save</li>
+                <li className='dropdown_item' onClick={() => { onOpen() }}>Open</li>
                 <li className='dropdown_item'>
-                    <button className='export_btn' onClick={() => {exportPng();}}>Export PNG</button>
+                    <button className='export_btn' onClick={() => { exportPng() }}>Export PNG</button>
                 </li>
+                <li onClick={() => { exportPng() }}>Export PNG</li>
             </ul>
         </>
     );
 }
 
-export function EditDropdown() {
+export function EditDropdown(props) {
+    const [dropdown, setDropdown] = useState(false);
     return (
         <>
-            <ul className='dropdown'>
+            <ul className={dropdown ? "dropdown clicked" : "edit_dropdown"} onClick={() => setDropdown(!dropdown)}>
                 <li className='dropdown_item'>Copy</li>
                 <li className='dropdown_item'>Cut</li>
                 <li className='dropdown_item'>Paste</li>
--- src/menubar/Menubar.js
+++ src/menubar/Menubar.js
@@ -1,26 +1,27 @@
 import './Menubar.css';
 import React, {useState} from 'react';
-import { FileDropdown } from './Dropdown';
+import { FileDropdown, EditDropdown } from './Dropdown';
 
 export default function Menubar(props) {
-    const [dropdown, setDropdown] = useState(false);
+    const [fileDropdown, setFileDropdown] = useState(false);
+    const [editDropdown, setEditDropdown] = useState(false);
     return (
         <>
             <nav className='menubar'>
                 <ul className='bar_items'>
                     <div>
-                        <li className='bar_file' onClick={() => setDropdown(!dropdown)} 
-                        >File
-                        {dropdown &&
+                        <li className='bar_file' onClick={() => setFileDropdown(!fileDropdown)}>File</li>
+                        {fileDropdown &&
                             <FileDropdown
                                 onSave={props.onSave}
                                 onOpen={props.onOpen}
                                 exportPng = {props.exportPng}
                             />
                         }
-                        </li>
-
-                        <li className='bar_edit'>Edit</li>
+                        <li className='bar_edit' onClick={() => setEditDropdown(!editDropdown)}>Edit</li>
+                        {editDropdown &&
+                            <EditDropdown/>
+                        }
                     </div>
                     <li className='bar_name'>CircuitCraft</li>
                     <li className='bar_toolbox'>Toolbox</li>
